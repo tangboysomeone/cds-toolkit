e85dcbbb1ce1b8d330439a2082a8b213d238aeea
tangboysomeone <1723425825@qq.com>
2014-06-18 16:30:20 +0800

    优化代码

--- com/wangyin/cds/toolkit/rest/cds.py
+++ com/wangyin/cds/toolkit/rest/cds.py
@@ -12,116 +12,105 @@ class CdsClient:
         self.port = rest_port
         self.event_url = 'http://{0}:{1}'.format(ip, event_port)
         self.url_prefix = 'http://{0}:{1}'.format(ip, rest_port)
-        self.headers = {'Host': '192.168.1.101', 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip', 'Poll-Req-Type': 'Req-Check-event'}
+        self.headers = {'Host': '192.168.1.101', 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip',
+                        'Poll-Req-Type': 'Req-Check-event'}
 
     def get_events(self):
         try:
-            r = requests.get(self.event_url, headers=self.headers)
-            self.headers['poll-session-id'] = r.headers['poll-session-id']
-            events = json.loads(r.text)
+            resp = requests.get(self.event_url, headers=self.headers)
+            self.headers['poll-session-id'] = resp.headers['poll-session-id']
+            events = json.loads(resp.text)
             return events
         except RequestException as ex:
             print 'Fail to connect cds_server', ex
         else:
             return None
 
-    def _url_format(self, template, **kwargs):
-        return self.url_prefix + template.format(kwargs)
-
     def get_cluster(self, cluster_name):
-        # URL = self.url_prefix + URLS.CLUSTER.format(cluster_name)
-        r = requests.get(self._url_format(self, URLS.CLUSTER, cluster_name))
-        if not r.text:
-            return None
-        cluster = json.loads(r.text)
-        if len(cluster) != 0:
-            cluster = DbCluster(cluster['id'], cluster['clusterName'])
-            groups = self.get_group(cluster_name)
-            for group in groups:
-                print group.type
-                if group.type == 'global':
-                    cluster.set_global_group(group)
-                if group.type == 'worker':
-                    cluster.add_worker_group(group)
-        print 'di', len(cluster)
-        return cluster if len(cluster) == 0 else None
+        resp = requests.get(self.url_prefix + URLS.CLUSTER.format(cluster_name))
 
-    def get_group(self, clusterName):
-        URL = self.url_prefix + URLS.DBGROUPBY_CLUSTER.format(clusterName)
-        r = requests.get(URL)
-        groups = json.loads(r.text)
-        retGroups = []
-        if len(groups) != 0:
-            for group in groups:
-                group = DbGroup(group['id'], group['groupName'], group['groupType'])
-                dbinfos = self.get_dbinfo(group.name)
-                for dbinfo in dbinfos:
-                    group.add_space(dbinfo, dbinfo.isMaster)
-                retGroups.append(group)
-        return retGroups
+        if not resp.text:
+            return None
+
+        cluster = json.loads(resp.text)
+        db_cluster = DbCluster(cluster['id'], cluster['clusterName'])
+        groups = self.get_group(cluster_name)
+
+        for group in groups:
+            if group.type == 'global':
+                db_cluster.set_global_group(group)
+            if group.type == 'worker':
+                db_cluster.add_worker_group(group)
+        return db_cluster
+
+    def get_group(self, cluster_name):
+        resp = requests.get(self.url_prefix + URLS.DBGROUPBY_CLUSTER.format(cluster_name))
+
+        if not resp.text:
+            return None
+
+        db_groups = []
+        for group in json.loads(resp.text):
+            db_group = DbGroup(group['id'], group['groupName'], group['groupType'])
+            db_infos = self.get_dbinfo(db_group.name)
+            for db_info in db_infos:
+                db_group.add_space(db_info, db_info.is_master)
+            db_groups.append(db_group)
+
+        return db_groups
 
     def get_dbinfo(self, group_name):
-        URL = self.url_prefix + URLS.DBINFO_GROUP_NAME.format(group_name)
-        r = requests.get(URL)
-        dbinfos = json.loads(r.text)
-        retDbinfos = []
-        if len(dbinfos) != 0:
-            for dbinfo in dbinfos:
-                dbinfo = DbInfo(dbinfo['id'], dbinfo['ip'], dbinfo['port'], dbinfo['dbName'], dbinfo['masterOrSlave'] == 'Master')
-                retDbinfos.append(dbinfo)
-        return retDbinfos
+        resp = requests.get(self.url_prefix + URLS.DBINFO_GROUP_NAME.format(group_name))
+        if not resp.text:
+            return None
+
+        ret_db_infos = []
+
+        for db_info in json.loads(resp.text):
+            db_info = DbInfo(db_info['id'], db_info['ip'], db_info['port'], db_info['dbName'],
+                             db_info['masterOrSlave'] == 'Master')
+            ret_db_infos.append(db_info)
+
+        return ret_db_infos
 
     def get_splitkey(self, cluster_id):
-        URL = self.url_prefix + URLS.SPLITTINGKEY_BY_CLUSTER_ID.format(cluster_id)
-        r = requests.get(URL)
-        keys = json.loads(r.text)
-        retKeys = []
-        if len(keys) != 0:
-            for key in keys:
-                key = SplittingKey(key['id'], key['splitName'], key['clusterId'])
-                retKeys.append(key)
-        return retKeys
+        resp = requests.get(self.url_prefix + URLS.SPLITTINGKEY_BY_CLUSTER_ID.format(cluster_id))
+        if not resp.text:
+            return None
+
+        split_keys = []
+        for split_key in json.loads(resp.text):
+            key = SplittingKey(split_key['id'], split_key['splitName'], split_key['clusterId'])
+            split_keys.append(key)
+
+        return split_keys
 
     def get_cloumn(self, key_id):
-        URL = self.url_prefix + URLS.COLUMN_BY_SPLITKEY.format(key_id)
-        r = requests.get(URL)
-        columns = json.loads(r.text)
-        retCols = []
-        if len(columns) != 0:
-            for column in columns:
-                columnn = ColumnInfo(column['id'], column['splittingKeyId'], column['table'], column['column'])
-                retCols.append(columnn)
-        return retCols
+        resp = requests.get(self.url_prefix + URLS.COLUMN_BY_SPLITKEY.format(key_id))
+        if not resp.text:
+            return None
+
+        return [ColumnInfo(column['id'], column['splittingKeyId'], column['table'], column['column'])
+                    for column in json.loads(resp.text)]
 
     def get_tblrule(self, key_id):
-        URL = self.url_prefix + URLS.RULES_BYSPLITKEY.format(key_id)
-        r = requests.get(URL)
-        depotRules = json.loads(r.text)
-        retRules = []
-        if len(depotRules) != 0:
-            for depotRule in depotRules:
-                retRules.append((int(depotRule['dbGroupId']), depotRule['tableSuffix']))
-        return retRules
+        resp = requests.get(self.url_prefix + URLS.RULES_BYSPLITKEY.format(key_id))
+        if not resp.text:
+            return None
 
-    def get_grp2tblsuf(self, clusterName, tableName):
-        cluster = self.get_cluster(clusterName)
-        spliKeys = self.get_splitkey(cluster.id)
-        keyIds = []
-        for spliKey in spliKeys:
-            columns = self.get_cloumn(spliKey.id)
-            keyIds.extend(self.find_splitkey(columns, tableName))
-        if keyIds:
-            gpId2Tbs = self.get_tblrule(keyIds[0])
-            return gpId2Tbs
-        # print gpId2Tbs
-        return None
+        return [(int(tbl_rule['dbGroupId']), tbl_rule['tableSuffix'])
+                    for tbl_rule in json.loads(resp.text)]
 
-    def find_splitkey(self, columns, table):
-        keys = []
-        for col in columns:
-            if col.table == table:
-                keys.append(col.splitKeyId)
-        return keys
+    def get_grpid2_tblsuf(self, cluster_name, table_name):
+        db_cluster = self.get_cluster(cluster_name)
+        split_keys = self.get_splitkey(db_cluster.id)
+
+        key_ids = []
+        for split_key in split_keys:
+            columns = self.get_cloumn(split_key.id)
+            key_ids.extend(column.splitKeyId for column in columns if column.table == table_name)
+
+        return self.get_tblrule(key_ids[0]) if len(key_ids) != 0 else None
 
 
 class URLS:
